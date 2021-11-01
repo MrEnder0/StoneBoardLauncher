1eb82dc5ff66a8967ed686e95dde1a0ed59d9026
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-11-01 13:44:07 -0500

    Update run.py

--- run.py
+++ run.py
@@ -5,3 +5,10 @@ from config import *
 dpackage = threading.Thread(target=downloadPackage, args=(1,), daemon=True)
 dpackage.start()
 startLauncher()
+
+if experimental:
+  from experimental.StoneBoardPackagesexperimental.home import *
+  startHome()
+else:
+  from stable.StoneBoardPackagesstable.home import *
+  startHome()
