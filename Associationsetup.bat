e92378e7b3274c5f707420c5cfedd8fef488bf4a
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-11-27 18:21:18 -0600

    WIP
    currently being worked on

--- Associationsetup.bat
+++ Associationsetup.bat
@@ -4,4 +4,4 @@ assoc .sbsave=StoneBoardSave
 assoc .sbtheme=StoneBoardTheme
 
 ftype StoneBoardSave="C:\Users\Owner\AppData\Local\Programs\Python\PYTHON36\python.exe" "C:\Users\Owner\Desktop\StoneBoardLauncher-main\importManager.py" "%1" %*
-ftype StoneBoardTheme="C:\Users\Owner\AppData\Local\Programs\Python\PYTHON36\python.exe" C:\Users\Owner\Desktop\StoneBoardLauncher-main\importManager.py" "%1" %*
+ftype StoneBoardTheme="C:\Users\Owner\AppData\Local\Programs\Python\PYTHON36\python.exe" "C:\Users\Owner\Desktop\StoneBoardLauncher-main\importManager.py" "%1" %*
